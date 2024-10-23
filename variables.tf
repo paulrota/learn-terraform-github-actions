6f5866c819800ae0c25694a8bcd89a0a86323082
paulrota <paul.rotariu24@yahoo.com>
2024-10-23 22:45:33 +0300

    upd: add variabled.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "AWS_ACCESS_KEY_ID" {
+  type        = string
+  description = "The AWS Access Key"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type        = string
+  description = "The AWS Secret Key"
+}
